ed478c73bc685772dfea3c96e98a96c51c05d120
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2018-12-30 18:09:40 -0200

    Added dataframe persistence to json

--- src/main/scala/org/codecraftlabs/spark/utils/DataUtils.scala
+++ src/main/scala/org/codecraftlabs/spark/utils/DataUtils.scala
@@ -6,4 +6,8 @@ object DataUtils {
   def saveDataSetToCsv(ds: DataFrame, partitions: Int = 1, destination: String, saveMode: String = "overwrite", header: Boolean = true) : Unit = {
     ds.coalesce(partitions).write.mode(saveMode).option("header", header).csv(destination)
   }
+
+  def saveDataFrameToJson(df: DataFrame, destination: String, partitions: Int = 1, saveMode: String = "overwrite", header: Boolean = false): Unit = {
+    df.coalesce(partitions).write.mode(saveMode).json(destination)
+  }
 }
