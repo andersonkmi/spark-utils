1c69bfaea88fe9972108017ab753693a9845d6cb
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2019-11-24 12:27:09 +0100

    refactored testing code

--- src/test/scala/org/codecraftlabs/spark/utils/ArgsUtilSpec.scala
+++ src/test/scala/org/codecraftlabs/spark/utils/ArgsUtilSpec.scala
@@ -1,34 +1,35 @@
 package org.codecraftlabs.spark.utils
 
+import org.codecraftlabs.spark.utils.ArgsUtils.parseArgs
 import org.scalatest.{BeforeAndAfterAll, FlatSpec, Matchers}
 
 class ArgsUtilSpec extends FlatSpec with Matchers with BeforeAndAfterAll {
 
   "Null args" should "be not null" in {
-    val result = ArgsUtils.parseArgs(null)
+    val result = parseArgs(null)
     result should not be null
   }
 
   "Null args" should "be empty" in {
-    val result = ArgsUtils.parseArgs(null)
+    val result = parseArgs(null)
     result.size shouldEqual 0
   }
 
   "Empty args" should "be empty" in {
     val args = Array.empty[String]
-    val result = ArgsUtils.parseArgs(args)
+    val result = parseArgs(args)
     result should not be null
     result.size shouldEqual 0
   }
 
   "Single args" should "return 1 empty arg" in {
     val args = Array("--test")
-    an [InvalidArgsException] should be thrownBy ArgsUtils.parseArgs(args)
+    an [InvalidArgsException] should be thrownBy parseArgs(args)
   }
 
   "Single arguments" should "return 1 valid arg" in {
     val args = Array("--test", "value")
-    val result = ArgsUtils.parseArgs(args)
+    val result = parseArgs(args)
     result should not be null
     result.size shouldEqual 1
     result.head._1 shouldEqual "--test"
@@ -37,7 +38,7 @@ class ArgsUtilSpec extends FlatSpec with Matchers with BeforeAndAfterAll {
 
   "Double arguments" should "return 2 valid arguments" in {
     val args = Array("--test", "value", "--test2", "value2")
-    val result = ArgsUtils.parseArgs(args)
+    val result = parseArgs(args)
     result should not be null
     result.size shouldEqual 2
     result.head._1 shouldEqual "--test"
