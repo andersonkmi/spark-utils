6bfc47019e2ba88b36d494e6f4e3054098927a3e
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2019-11-25 20:29:08 +0100

    refactoring

--- src/main/scala/org/codecraftlabs/spark/utils/FileUtil.scala
+++ src/main/scala/org/codecraftlabs/spark/utils/FileUtil.scala
@@ -7,28 +7,19 @@ object FileUtil {
   def getListOfFiles(dir: String): List[String] = {
     val root = new File(dir)
 
-    if (root.exists && root.isDirectory) {
-      getRecursiveListOfFiles(root).map(item => item.getAbsolutePath).filter(item => item.endsWith(".csv") || item.endsWith(".parquet")).toList
-    } else {
-      List[String]()
-    }
+    if (!(root.exists && root.isDirectory)) List[String]() else getRecursiveListOfFiles(root).map(item => item.getAbsolutePath).filter(item => item.endsWith(".csv") || item.endsWith(".parquet")).toList
   }
 
   def getListOfFiles(dir: String, extensions: List[String]): List[String] = {
     val root = new File(dir)
 
-    if (root.exists && root.isDirectory) {
-      getRecursiveListOfFiles(root).filter(item => extensions.exists(item.getName.endsWith(_))).map(item => item.getAbsolutePath).toList
-    } else {
-      List[String]()
-    }
+    if (!(root.exists && root.isDirectory)) List[String]() else getRecursiveListOfFiles(root).filter(item => extensions.exists(item.getName.endsWith(_))).map(item => item.getAbsolutePath).toList
   }
 
   def getRecursiveListOfFiles(dir: File): Array[File] = {
     val these = dir.listFiles
     these ++ these.filter(_.isDirectory).flatMap(getRecursiveListOfFiles)
   }
-
 
   def buildFilePath(folder: String, fileName: String): String = {
     s"$folder" + getDefault.getSeparator + fileName
