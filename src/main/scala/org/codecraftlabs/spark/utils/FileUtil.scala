b20899a0d19ea173fcec534e2d9544ec2f101b02
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2019-12-01 21:52:33 +0100

    modifications

--- src/main/scala/org/codecraftlabs/spark/utils/FileUtil.scala
+++ src/main/scala/org/codecraftlabs/spark/utils/FileUtil.scala
@@ -4,13 +4,15 @@ import java.io.File
 import java.nio.file.FileSystems.getDefault
 
 object FileUtil {
-  def getListOfFiles(dir: String, extensions: List[String]): List[String] = {
-    val root = new File(dir)
-
-    if (!(root.exists && root.isDirectory)) List[String]() else getRecursiveListOfFiles(root).filter(item => extensions.exists(item.getName.endsWith(_))).map(item => item.getAbsolutePath).toList
+  def getListOfFiles(dir: Option[String], extensions: List[String]): List[String] = {
+    dir match {
+      case Some(directory) => val root = new File (directory)
+        if (! (root.exists && root.isDirectory) ) List[String]() else getRecursiveListOfFiles (root).filter (item => extensions.exists (item.getName.endsWith (_) ) ).map (item => item.getAbsolutePath).toList
+      case None => List[String]()
+    }
   }
 
-  def getRecursiveListOfFiles(dir: File): Array[File] = {
+  private def getRecursiveListOfFiles(dir: File): Array[File] = {
     val these = dir.listFiles
     these ++ these.filter(_.isDirectory).flatMap(getRecursiveListOfFiles)
   }
