8ff318259882b852fd19aadae008dd943e1ed62e
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2024-02-04 14:45:45 +0100

    Improved unit test

--- src/test/scala/org/codecraftlabs/spark/utils/FileUtilSpec.scala
+++ src/test/scala/org/codecraftlabs/spark/utils/FileUtilSpec.scala
@@ -1,12 +1,29 @@
 package org.codecraftlabs.spark.utils
 
 import org.codecraftlabs.spark.utils.FileUtil.getListOfFiles
+import org.scalatest.BeforeAndAfterAll
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
+import java.io.File
 import scala.reflect.io.Path
 
-class FileUtilSpec extends AnyFlatSpec with Matchers {
+class FileUtilSpec extends AnyFlatSpec with Matchers with BeforeAndAfterAll {
+  private val CurrentDirectory = Path("src/test/scala/org/codecraftlabs/spark/utils").toAbsolute
+  private val EmptyFile1 = CurrentDirectory.toString + "/empty1.txt"
+  private val EmptyFile2 = CurrentDirectory.toString + "/empty2.txt"
+  private val ExpectedFiles = Set(CurrentDirectory.toString() + "/ArgsUtilSpec.scala", CurrentDirectory.toString() + "/FileUtilSpec.scala")
+
+  override def beforeAll(): Unit = {
+    createEmptyFile(EmptyFile1)
+    createEmptyFile(EmptyFile2)
+  }
+
+  override def afterAll(): Unit = {
+    removeFile(EmptyFile1)
+    removeFile(EmptyFile2)
+  }
+
   "When directory option is empty" should "return empty list" in {
     val results = getListOfFiles(None, List(".scala"))
     results should not be None
@@ -14,14 +31,25 @@ class FileUtilSpec extends AnyFlatSpec with Matchers {
   }
 
   "When directory is ." should "return test case files" in {
-    val currentDirectory = Path("src/test/scala/org/codecraftlabs/spark/utils").toAbsolute
-    val results = getListOfFiles(Some(currentDirectory.toString()), List(".scala"))
+    val results = getListOfFiles(Some(CurrentDirectory.toString()), List(".scala"))
     results should not be None
     results should not be Set.empty
 
     // Asserting the file names
-    val expectedFileNames: Set[String] = Set(currentDirectory.toString() + "/ArgsUtilSpec.scala",
-      currentDirectory.toString() + "/FileUtilSpec.scala")
-    expectedFileNames.subsetOf(results) shouldEqual true
+    ExpectedFiles.subsetOf(results) shouldEqual true
+  }
+
+  def createEmptyFile(filePath: String): Unit = {
+    val file = new File(filePath)
+    if (file.createNewFile()) {
+      println(s"File created: ${file.getName}")
+    } else {
+      println("File already exists.")
+    }
+  }
+
+  def removeFile(filePath: String): Unit = {
+    val file = new File(filePath)
+    file.delete();
   }
 }
