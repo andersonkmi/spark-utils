190a6935d803951bf47b63566bab40580a734a5e
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2024-04-05 08:48:37 +0200

    Add unit test to save df in parquet format

--- src/test/scala/org/codecraftlabs/spark/utils/DataFrameUtilSpec.scala
+++ src/test/scala/org/codecraftlabs/spark/utils/DataFrameUtilSpec.scala
@@ -4,7 +4,7 @@ import org.apache.spark.SparkConf
 import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
 import org.apache.spark.sql.functions.asc
 import org.apache.spark.sql.{DataFrame, Row, SparkSession}
-import org.codecraftlabs.spark.utils.DataFrameUtil.{extractDistinctValues, saveDataFrameToCsv, saveDataFrameToJson}
+import org.codecraftlabs.spark.utils.DataFrameUtil.{extractDistinctValues, saveDataFrameToCsv, saveDataFrameToJson, saveDataFrameToParquet}
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
@@ -16,6 +16,7 @@ class DataFrameUtilSpec extends AnyFlatSpec with Matchers with BeforeAndAfterAll
   private val CurrentDirectory = Path("src/test/scala/org/codecraftlabs/spark/utils").toAbsolute
   private val CsvFile = CurrentDirectory.toString + "/dataframe.csv"
   private val JsonFile = CurrentDirectory.toString + "/dataframe.json"
+  private val ParquetFile = CurrentDirectory.toString + "/dataframe.parquet"
 
   override def beforeAll(): Unit = {
     val sparkConfig = new SparkConf()
@@ -53,6 +54,11 @@ class DataFrameUtilSpec extends AnyFlatSpec with Matchers with BeforeAndAfterAll
   "When saving the data frame as json" should "create a json file" in {
     val dataFrame = createDataFrame()
     saveDataFrameToJson(df = dataFrame, destination = JsonFile)
+  }
+
+  "When saving the data frame as parquet" should "create a parquet file" in {
+    val dataFrame = createDataFrame()
+    saveDataFrameToParquet(df = dataFrame, destination = ParquetFile)
   }
 
   "When extracting distinct values" should "return a new dataframe with unique values" in {
